3fb1266be2d6fb19da36d874dd6382140f61381a
Alexander Popov <alex.wayfer@gmail.com>
2020-03-30 00:51:43 +0300

    Add flow (main functionality)

---
+++ lib/twitch_oauth2/client.rb
@@ -0,0 +1,109 @@
+# frozen_string_literal: true
+
+require 'faraday'
+require 'securerandom'
+
+module TwitchOAuth2
+	## Client for making requests
+	class Client
+		CONNECTION = Faraday.new(
+			url: 'https://id.twitch.tv/oauth2/'
+		) do |connection|
+			connection.request :url_encoded
+		end.freeze
+
+		def initialize(client_id:, client_secret:, redirect_uri: nil, scopes: nil)
+			@client_id = client_id
+			@client_secret = client_secret
+			@redirect_uri = redirect_uri
+			@scopes = scopes
+
+			@state = SecureRandom.alphanumeric(32)
+		end
+
+		def flow(scopes: @scopes, access_token: nil, refresh_token: nil)
+			if access_token
+				result = validate access_token: access_token
+
+				return refresh(refresh_token: refresh_token) if result[:status] == 401
+
+				return result if result[:expires_in].positive?
+			end
+
+			flow_with_authorize scopes: scopes
+		end
+
+		private
+
+		def flow_with_authorize(scopes:)
+			link = authorize scopes: scopes
+
+			puts <<~TEXT
+				1. Open URL in your browser:
+					#{link}
+				2. Login to Twitch.
+				3. Copy the `code` parameter from redirected URL.
+				4. Insert below:
+			TEXT
+
+			code = STDIN.gets.chomp
+
+			token code: code
+		end
+
+		def authorize(scopes: @scopes)
+			scope = transform_scopes_to_string scopes
+
+			response = CONNECTION.get(
+				'authorize',
+				client_id: @client_id,
+				redirect_uri: @redirect_uri,
+				scope: scope,
+				response_type: :code
+			)
+
+			response.headers[:location]
+		end
+
+		def token(code:)
+			response = CONNECTION.post(
+				'token',
+				client_id: @client_id,
+				client_secret: @client_secret,
+				code: code,
+				grant_type: :authorization_code,
+				redirect_uri: @redirect_uri
+			)
+
+			parse_json_response response.body
+		end
+
+		def validate(access_token:)
+			response = CONNECTION.get(
+				'validate', {}, { 'Authorization' => "OAuth #{access_token}" }
+			)
+
+			parse_json_response response.body
+		end
+
+		def refresh(refresh_token:)
+			response = CONNECTION.post(
+				'token',
+				client_id: @client_id,
+				client_secret: @client_secret,
+				grant_type: :refresh_token,
+				refresh_token: refresh_token
+			)
+
+			parse_json_response response.body
+		end
+
+		def transform_scopes_to_string(scopes)
+			Array(scopes).join(' ')
+		end
+
+		def parse_json_response(string)
+			JSON.parse(string).transform_keys(&:to_sym)
+		end
+	end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,53 @@ if ENV['CODECOV_TOKEN']
 	SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
+ENV['TWITCH_CLIENT_ID'] ||= 'test_client_id'
+ENV['TWITCH_CLIENT_SECRET'] ||= 'test_client_secret'
+ENV['TWITCH_ACCESS_TOKEN'] ||= 'test_access_token'
+ENV['TWITCH_REFRESH_TOKEN'] ||= 'test_refresh_token'
+
+require 'vcr'
+
+VCR.configure do |config|
+	config.cassette_library_dir = "#{__dir__}/cassettes"
+	config.default_cassette_options = { record_on_error: false }
+	config.hook_into :faraday
+	config.configure_rspec_metadata!
+
+	config.filter_sensitive_data('<CLIENT_ID>') do
+		ENV['TWITCH_CLIENT_ID']
+	end
+
+	config.filter_sensitive_data('<CLIENT_SECRET>') do
+		ENV['TWITCH_CLIENT_SECRET']
+	end
+
+	config.filter_sensitive_data('<ACTUAL_ACCESS_TOKEN>') do
+		ENV['TWITCH_ACCESS_TOKEN']
+	end
+
+	config.filter_sensitive_data('<ACTUAL_REFRESH_TOKEN>') do
+		ENV['TWITCH_REFRESH_TOKEN']
+	end
+
+	config.filter_sensitive_data('<CODE>') do |interaction|
+		URI.decode_www_form(interaction.request.body).to_h['code']
+	end
+
+	config.filter_sensitive_data('<ACCESS_TOKEN>') do |interaction|
+		if interaction.response.headers['content-type'].include? 'application/json'
+			JSON.parse(interaction.response.body)['access_token']
+		end
+	end
+
+	config.filter_sensitive_data('<REFRESH_TOKEN>') do |interaction|
+		if interaction.response.headers['content-type'].include? 'application/json'
+			JSON.parse(interaction.response.body)['refresh_token']
+		end
+	end
+end
+
 require_relative '../lib/twitch_oauth2'
+
+# TwitchOAuth2::Client::CONNECTION
+# 	.response :logger, nil, { headers: true, bodies: true }
