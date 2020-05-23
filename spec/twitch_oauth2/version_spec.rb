e7a85c97089de6bbd1e5a5e62c4c3d3784f5678c
Alexander Popov <alex.wayfer@gmail.com>
2020-05-23 19:12:28 +0300

    Update RuboCop to version 0.83

--- spec/twitch_oauth2/version_spec.rb
+++ spec/twitch_oauth2/version_spec.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
-## `described_class` is `nil` when `describe` with `String`
-# rubocop:disable RSpec/DescribeClass
 describe 'TwitchOAuth2::VERSION' do
-	# rubocop:enable RSpec/DescribeClass
 	subject { Object.const_get(self.class.description) }
 
 	it { is_expected.to match(/\d+\.\d+\.\d+/) }
